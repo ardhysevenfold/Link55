383a7302ea57915f054c44d75e1c28f6207c48f4
ardhysevenfold <39523322+ardhysevenfold@users.noreply.github.com>
2026-03-02 01:34:44 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,20 @@
 export default async function handler(req, res) {
   try {
-    return res.status(200).json({
-      status: "API aktif",
-      waktu: new Date().toISOString()
+
+    const emerald = await fetch("https://newapi.55lottertttapi.com/api/webapi/GetNoaverageEmerdList");
+    const emeraldData = await emerald.json();
+
+    const gameissue = await fetch("https://newapi.55lottertttapi.com/api/webapi/GetGameIssue");
+    const gameissueData = await gameissue.json();
+
+    res.status(200).json({
+      periode: gameissueData.periode,
+      hasil: emeraldData.hasil
     });
+
   } catch (err) {
-    return res.status(500).json({ error: "Error server" });
+    res.status(500).json({
+      error: "Gagal ambil data"
+    });
   }
 }
